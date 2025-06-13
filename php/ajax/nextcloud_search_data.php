d9d24689341ba0756b70c9bfe04d83dab235064a
Benno <b.perceval@superceval.fr>
2025-06-13 16:20:55 +0200

    Update nextcloud_search_data.php

--- php/ajax/nextcloud_search_data.php
+++ php/ajax/nextcloud_search_data.php
@@ -4,7 +4,11 @@ include '../properties.php';
 $dbconn_geo = pg_connect("hostaddr=$DBHOST_geonature port=$PORT_geonature dbname=$DBNAME_geonature user=$LOGIN_geonature password=$PASS_geonature") or die ('Connexion impossible :'. pg_last_error());
 $dbconn_nx = pg_connect("hostaddr=$DBHOST_nextcloud port=$PORT_nextcloud dbname=$DBNAME_nextcloud user=$LOGIN_nextcloud password=$PASS_nextcloud") or die ('Connexion impossible :'. pg_last_error());
 
-$delete = pg_prepare($dbconn_geo, "sql", "DELETE * FROM $nx_users;");
+///////////////////////////////////////////////////////////////////
+// A. Insertion des nouveaux utilisateurs dans la table nextcloud.users
+///////////////////////////////////////////////////////////////////
+
+$delete = pg_prepare($dbconn_geo, "sql", "DELETE FROM $nx_users;");
 $delete = pg_execute($dbconn_geo, "sql",array()) or die ( pg_last_error());
 
 
@@ -24,15 +28,38 @@ where b_.email is not null
 group by 1,2,3
 order by 3
 ");
+//PREPARE ONE INSERT
+$insert = pg_prepare($dbconn_geo, "sql_insert", "INSERT INTO $nx_users (uuid_nx, nom_ad, courriel) VALUES ($1, $2, $3);");
+//EXECUTE LES INSERT
 $insert_s = pg_execute($dbconn_nx, "sql",array()) or die ( pg_last_error());
 while($row = pg_fetch_row($insert_s))
 {
-  $insert = pg_prepare($dbconn_geo, "sql_insert", "INSERT INTO $nx_users (uuid_nx, nom_ad) VALUES ($1, $2);");
-  $insert = pg_execute($dbconn_geo, "sql_insert",array($row[0], $row[1])) or die ( pg_last_error());
+  $insert = pg_execute($dbconn_geo, "sql_insert",array($row[0], $row[1], $row[2])) or die ( pg_last_error());
   echo $row[0].' - '.$row[1].' - '.$row[2].'</br>';
 }
 
+///////////////////////////////////////////////////////////////////
+// B. Mise à jour du champ gn_user_name dans la table nextcloud.users
+///////////////////////////////////////////////////////////////////
+$update = pg_prepare($dbconn_geo, "sql_update", "
+with a_ as (select courriel, nom_ad, id_role, email from nextcloud.users 
+left join utilisateurs.t_roles on t_roles.email = courriel
+group by 1,2,3,4
+order by 1
+)
+update nextcloud.users set gn_user_name = a_.id_role from a_ where a_.courriel = users.email;
+");
+$update_ = pg_execute($dbconn_geo, "sql_update",array()) or die ( pg_last_error());
 
+///////////////////////////////////////////////////////////////////
+// C. Mise à jour du champ uuid_nx dans la table suivi_faune
+///////////////////////////////////////////////////////////////////
+
+/* $file_size = pg_prepare($dbconn_geo, "sql_insert", "SELECT ");
+$observations_gpkg = '/var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/observations.gpkg';
+  //if file_exists
+  if (file_exists($observations_gpkg)) {
+  } */
 
 /* 						$db = new SQLite3('/var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/observations.gpkg');
 						$db->loadExtension('mod_spatialite.so');
