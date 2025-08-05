624278781b0db339bd1bb3a8ed5301db021d2795
Benno <b.perceval@superceval.fr>
2025-08-05 17:22:19 +0200

    Update write_table_from_skeleton.js.php

--- php/ajax/write_table_from_skeleton.js.php
+++ php/ajax/write_table_from_skeleton.js.php
@@ -1,6 +1,12 @@
 <?php
 include '../properties.php';
 
+$table = $_POST['table_name'];
+if (empty($table)) {
+    echo "Table name is required.";
+    exit();
+}
+
 $dbconn_geo = pg_connect("hostaddr=$DBHOST_geonature port=$PORT_geonature dbname=$DBNAME_geonature user=$LOGIN_geonature password=$PASS_geonature") or die ('Connexion impossible :'. pg_last_error());
 $dbconn_nx = pg_connect("hostaddr=$DBHOST_nextcloud port=$PORT_nextcloud dbname=$DBNAME_nextcloud user=$LOGIN_nextcloud password=$PASS_nextcloud") or die ('Connexion impossible :'. pg_last_error());
 
