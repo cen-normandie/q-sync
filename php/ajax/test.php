ac78c126f020406bf8fad7386b25662f0b102112
Benno <b.perceval@superceval.fr>
2025-08-06 09:49:42 +0200

    Update test.php

--- php/ajax/test.php
+++ php/ajax/test.php
@@ -1,6 +1,6 @@
 <?php
-exec("sqlite3 /var/www/html/q-sync/_qfield_skeleton/observations.gpkg '.dump meta_qsync' > /var/www/html/q-sync/meta_qsync.sql", $output, $retval);
-echo "</br>Export depuis observations.gpkg SKELETON with status $retval and output:\n";
+exec("sqlite3 /var/www/html/q-sync/_qfield_skeleton/observations.gpkg '.dump meta_qsync' > /var/www/html/q-sync/_qfield_skeleton/dumps/meta_qsync.sql", $output, $retval);
+echo "</br>Export depuis observations.gpkg SKELETON with status $retval and output:";
 print_r($output);
 
 
