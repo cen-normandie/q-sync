4eb4dcd5144ef3d7b1130ee0338d173aca0b0e69
Benno <b.perceval@superceval.fr>
2025-08-08 16:10:36 +0200

    Update import_faune.js.php

--- php/ajax/import_faune.js.php
+++ php/ajax/import_faune.js.php
@@ -13,15 +13,15 @@ while($row = pg_fetch_row($personne))
   $observations_gpkg = '/var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/observations.gpkg';
   //if file_exists
   if (file_exists($observations_gpkg)) {
-		$cmd='ogr2ogr -f PostgreSQL "PG:user='.$LOGIN_geonature.' host='.$DBHOST_geonature.' dbname='.$DBNAME_geonature.' password='.$PASS_geonature.'" /var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/observations.gpkg -nln sandbox.obs_faune -append -sql "SELECT *, \''.$row[1].'\' as courriel, \''.$row[3].'\' as uuid_nx from obs_faune where date_import is null" 2>&1';
+		//Lit toutes les obs faune non importées depuis les geopackage et les envoies dans postgres sandbox.faune
+		$cmd='ogr2ogr -f PostgreSQL "PG:user='.$LOGIN_geonature.' host='.$DBHOST_geonature.' dbname='.$DBNAME_geonature.' password='.$PASS_geonature.'" /var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/observations.gpkg -nln '.$point_faune.' -append -sql "SELECT *, \''.$row[1].'\' as courriel, \''.$row[3].'\' as uuid_nx from '.$faune.' where date_import is null" 2>&1';
 		exec($cmd, $output);
 		if ($output != 2) {
-			/* pg_prepare($dbconn, "sql_u", "UPDATE $point_faune set uuid_nx = $1, courriel = $2  where uuid_nx is null;");
-            $update = pg_execute($dbconn, "sql_u",array($row[3], $row[1])) or die ( pg_last_error()); */
-			
+			//importe toutes les obs faune de sandbox.obs_faune dans geonature
 			pg_prepare($dbconn, "sql_import_occtax", "select sandbox.import_faune();");
 			$out = pg_execute($dbconn, "sql_import_occtax",array()) or die ( pg_last_error());
 			if ($out) {
+				//Pour chaque observation importée, on va mettre à jour la date d'import dans le geopackage
 				pg_prepare($dbconn, "sql_up", "SELECT id, uuid_nx, uuid_obs, date_import FROM $suivi_faune where gpkg_updated is false and uuid_nx = $1 ;");
 				$to_up = pg_execute($dbconn, "sql_up",array($row[3])) or die ( pg_last_error());
 				while($row_ = pg_fetch_row($to_up))
@@ -32,6 +32,7 @@ while($row = pg_fetch_row($personne))
 						$results_write_gpkg = $db->query("UPDATE obs_faune set date_import = datetime('now') where date_import is null and uuid_obs = '".$row_[2]."';"); //
 						pg_prepare($dbconn, "sql_down", "UPDATE $suivi_faune set gpkg_updated = true where uuid_obs = $1;");
 						if ($results_write_gpkg) {
+							//lance un scan des fichiers pour mettre à jour les geopackages de nextcloud
 							pg_execute($dbconn, "sql_down",array($row_[2])) or die ( pg_last_error());
 							$cmd2 = 'sudo -u www-data php /var/www/html/nextcloud/occ files:scan -p "'.$row[3].'"';
 							exec($cmd2, $output);
