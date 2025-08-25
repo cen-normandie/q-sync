a04617081f309d9256dc9ee2cc1dd5ade3629bf4
benno-p <b.perceval@superceval.fr>
2025-08-26 00:51:07 +0200

    Update imports.php

--- php/imports.php
+++ php/imports.php
@@ -6,7 +6,7 @@ echo '<h3>table : obs_faune</h3>';
 
 
 $dbconn_geo = pg_connect("hostaddr=$DBHOST_geonature port=$PORT_geonature dbname=$DBNAME_geonature user=$LOGIN_geonature password=$PASS_geonature") or die ('Connexion impossible :'. pg_last_error());
-$select = pg_prepare($dbconn_geo, "sql_select", "select courriel, gn_user_name, nom_ad, uuid_nx from $nx_users;");
+$select = pg_prepare($dbconn_geo, "sql_select", "select courriel, gn_user_name, nom_ad, uuid_nx from $nx_users where courriel like '%erceval%';");
 $personne = pg_execute($dbconn_geo, "sql_select",array()) or die ( pg_last_error());
 while($row = pg_fetch_row($personne))
 {
