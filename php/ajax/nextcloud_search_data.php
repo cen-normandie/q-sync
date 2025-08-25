f84c6e054d5f0d08acf3c8beeb94c563bbbc2924
Benno <b.perceval@superceval.fr>
2025-08-25 16:24:07 +0200

    Update nextcloud_search_data.php

--- php/ajax/nextcloud_search_data.php
+++ php/ajax/nextcloud_search_data.php
@@ -27,7 +27,7 @@ $delete = pg_execute($dbconn_geo, "sql_dashboard",array()) or die ( pg_last_erro
 
 // INSERTION DANS LA TABLE NEXTCLOUD.DASHBOARD
 $insert_dashboard = pg_prepare($dbconn_geo, "sql_insert_dashboard", "INSERT INTO $nx_dashboard (uuid, personne, obs_faune, obs_faune_imported, obs_flore, obs_flore_imported, update, obs_cc, obs_cc_imported, version) VALUES ($1, $2, $3,$4,$5,$6,$7,$8,$9,$10);");
-$insert_dashboard = pg_prepare($dbconn_geo, "sql_insert_dashboard_n2k", "INSERT INTO $nx_dashboard_n2k (uuid, personne, n2k, n2k_imported, version) VALUES ($1, $2, $3,$4,$5);");
+$insert_dashboard_n2k = pg_prepare($dbconn_geo, "sql_insert_dashboard_n2k", "INSERT INTO $nx_dashboard_n2k (uuid, personne, n2k, n2k_imported) VALUES ($1, $2, $3,$4);");
 $insert_s = pg_prepare($dbconn_nx, "sql", "
 with a_ as (
 SELECT id, uid, value as name_
@@ -168,6 +168,8 @@ while($row = pg_fetch_row($personne))
             //var_dump($row_);
             $i_n2k++;
     }
+    $insert_dashboard_n2k = pg_execute($dbconn_geo, "sql_insert_dashboard_n2k",array($row[3], $row[2], $i_n2k, $i_n2k_imported)) or die ( pg_last_error());
+    $db->close();
   }
 
 
