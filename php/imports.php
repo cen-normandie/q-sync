d9b89e05a1cbd94ab830e22d8da5c61b4323e59b
benno-p <b.perceval@superceval.fr>
2025-08-26 00:40:13 +0200

    Update imports.php

--- php/imports.php
+++ php/imports.php
@@ -5,27 +5,26 @@ echo '<h2>IMPORT DES OBSERVATIONS faune du serveur nextcloud dans le dossier UUI
 echo '<h3>table : obs_faune</h3>';
 
 
-$dbconn = pg_connect("hostaddr=$DBHOST port=$PORT dbname=$DBNAME user=$LOGIN password=$PASS") or die ('Connexion impossible :'. pg_last_error());
-$result = pg_prepare($dbconn, "sql", "SELECT id, courriel, gn_user_name, uuid_nx, active FROM $nx_users;");
-$personne = pg_execute($dbconn, "sql",array()) or die ( pg_last_error());
-
-
+$dbconn_geo = pg_connect("hostaddr=$DBHOST_geonature port=$PORT_geonature dbname=$DBNAME_geonature user=$LOGIN_geonature password=$PASS_geonature") or die ('Connexion impossible :'. pg_last_error());
+$select = pg_prepare($dbconn_geo, "sql_select", "select courriel, gn_user_name, nom_ad, uuid_nx from $nx_users;");
+$personne = pg_execute($dbconn_geo, "sql_select",array()) or die ( pg_last_error());
 while($row = pg_fetch_row($personne))
 {
-  echo $row[0].' - '.$row[1].' - '.$row[2].' - '.$row[3].' - '.$row[4];
-  //FAUNE + AUTRE POINT
   $observations_gpkg = '/var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/observations.gpkg';
   //if file_exists
   if (file_exists($observations_gpkg)) {
-		$cmd='ogr2ogr -f PostgreSQL "PG:user=postgres host=192.168.1.244 dbname=geonature password='.$PASS.'" /var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/observations.gpkg -nln sandbox.obs_faune -append -sql "SELECT *, \''.$row[1].'\' from obs_faune where date_import is null" 2>&1';
-		echo '</br>'.$cmd.'</br>';
-		exec($cmd, $output);
-		if (!$output) {
-			echo '<span style="font-weight:900;color:#056e15">DONE</span></br>';
-			pg_prepare($dbconn, "sql_u", "UPDATE $point_faune set uuid_nx = $1, courriel = $2  where uuid_nx is null;");
-			$update = pg_execute($dbconn, "sql_u",array($row[3], $row[1])) or die ( pg_last_error());
+    $cmd='ogr2ogr -f PostgreSQL "PG:user=postgres host=192.168.1.244 dbname=geonature password='.$PASS.'" /var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/observations.gpkg -nln sandbox.obs_faune -append -sql "SELECT *, \''.$row[1].'\' from obs_faune where date_import is null" 2>&1';
+    echo '</br>'.$cmd.'</br>';
+    exec($cmd, $output);
+    if (!$output) {
+        echo '<span style="font-weight:900;color:#056e15">DONE</span></br>';
+    } else {
+        echo '<span style="font-weight:900;color:#bc2020">FAILED</span></br>';
+    }
+  }
+}
 			
-			pg_prepare($dbconn, "sql_import_occtax", "select sandbox.import_faune();");
+/* 			pg_prepare($dbconn, "sql_import_occtax", "select sandbox.import_faune();");
 			$out = pg_execute($dbconn, "sql_import_occtax",array()) or die ( pg_last_error());
 			if ($out) {
 				pg_prepare($dbconn, "sql_up", "SELECT id, uuid_nx, uuid_obs, date_import FROM $suivi_faune where gpkg_updated is false and uuid_nx = $1 ;");
@@ -43,19 +42,11 @@ while($row = pg_fetch_row($personne))
 							exec($cmd2, $output);
 						}
 					}
-			}
+			} */
 			
-		} else {
-			echo '<span style="font-weight:900;color:#bc2020">FAILED</span></br>';
-			print_r($output);
-		}
-  } else {
-  echo '</br>NOT EXISTS</br>';
-  }
-}
 
 
-pg_close($dbconn);
+pg_close($dbconn_geo);
 
 
 
