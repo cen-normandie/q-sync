0545f7176ff8960f29dd9e2522d5362c53a20211
benno-p <b.perceval@superceval.fr>
2025-08-26 02:35:39 +0200

    Update imports.php

--- php/imports.php
+++ php/imports.php
@@ -8,6 +8,7 @@ echo '<h3>table : obs_faune</h3>';
 $dbconn_geo = pg_connect("hostaddr=$DBHOST_geonature port=$PORT_geonature dbname=$DBNAME_geonature user=$LOGIN_geonature password=$PASS_geonature") or die ('Connexion impossible :'. pg_last_error());
 $select = pg_prepare($dbconn_geo, "sql_select", "select courriel, gn_user_name, nom_ad, uuid_nx from $nx_users where courriel like '%erceval%';");
 $personne = pg_execute($dbconn_geo, "sql_select",array()) or die ( pg_last_error());
+pg_prepare($dbconn_geo, "sql_down", "UPDATE $suivi_point_faune set gpkg_updated = true where uuid_obs = $1;");
 while($row = pg_fetch_row($personne))
 {
   echo 'courriel :'.$row[0].'</br>';
@@ -37,7 +38,6 @@ while($row = pg_fetch_row($personne))
                     $db->loadExtension('mod_spatialite.so');
                     echo '</br>'."UPDATE $faune set date_import = datetime('now') where date_import is null and uuid_obs = '".$row_[2]."';".'</br>'; //
                     $results_write_gpkg = $db->query("UPDATE $faune set date_import = datetime('now') where date_import is null and uuid_obs = '".$row_[2]."';"); //
-                    pg_prepare($dbconn_geo, "sql_down", "UPDATE $suivi_point_faune set gpkg_updated = true where uuid_obs = $1;");
                     if ($results_write_gpkg) {
                         pg_execute($dbconn_geo, "sql_down",array($row_[2])) or die ( pg_last_error());
                         $cmd2 = 'sudo -u www-data php /var/www/html/nextcloud/occ files:scan -p "'.$row[3].'"';
