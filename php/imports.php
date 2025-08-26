526da3938b82cb4fc9175d472ddc9097c4a0092d
benno-p <b.perceval@superceval.fr>
2025-08-26 02:32:49 +0200

    Update imports.php

--- php/imports.php
+++ php/imports.php
@@ -24,27 +24,32 @@ while($row = pg_fetch_row($personne))
     exec($cmd, $output, $return_var);
     if ($return_var !== 2) {
         echo '<span style="font-weight:900;color:#056e15">DONE</span></br>';
-        pg_prepare($dbconn_geo, "sql_up", "SELECT id, uuid_nx, uuid_obs, date_import FROM $suivi_point_faune where gpkg_updated is false and uuid_nx = $1 ;");
-        echo '</br>select update : SELECT id, uuid_nx, uuid_obs, date_import FROM $suivi_point_faune where gpkg_updated is false and uuid_nx = \''.$row[3].'\' ;';
-        $to_up = pg_execute($dbconn_geo, "sql_up",array($row[3])) or die ( pg_last_error());
-        while($row_ = pg_fetch_row($to_up))
-            {
-                $db = new SQLite3('/var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/observations.gpkg');
-                $db->loadExtension('mod_spatialite.so');
-                echo '</br>'."UPDATE $faune set date_import = datetime('now') where date_import is null and uuid_obs = '".$row_[2]."';".'</br>'; //
-                $results_write_gpkg = $db->query("UPDATE $faune set date_import = datetime('now') where date_import is null and uuid_obs = '".$row_[2]."';"); //
-                pg_prepare($dbconn, "sql_down", "UPDATE $suivi_point_faune set gpkg_updated = true where uuid_obs = $1;");
-                if ($results_write_gpkg) {
-                    pg_execute($dbconn, "sql_down",array($row_[2])) or die ( pg_last_error());
-                    $cmd2 = 'sudo -u www-data php /var/www/html/nextcloud/occ files:scan -p "'.$row[3].'"';
-                    $output2=[];
-                    $return_var2=0;
-                    exec($cmd2, $output2, $return_var2);
-                    if ($return_var2 !== 2) {
-                        echo 'scan impossible -_-';
+        //importe toutes les obs faune de sandbox.obs_faune dans geonature
+        pg_prepare($dbconn, "sql_import_occtax", "select sandbox.import_faune();");
+        $out = pg_execute($dbconn, "sql_import_occtax",array()) or die ( pg_last_error());
+        if ($out) {
+            pg_prepare($dbconn_geo, "sql_up", "SELECT id, uuid_nx, uuid_obs, date_import FROM $suivi_point_faune where gpkg_updated is false and uuid_nx = $1 ;");
+            echo '</br>select update : SELECT id, uuid_nx, uuid_obs, date_import FROM $suivi_point_faune where gpkg_updated is false and uuid_nx = \''.$row[3].'\' ;';
+            $to_up = pg_execute($dbconn_geo, "sql_up",array($row[3])) or die ( pg_last_error());
+            while($row_ = pg_fetch_row($to_up))
+                {
+                    $db = new SQLite3('/var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/observations.gpkg');
+                    $db->loadExtension('mod_spatialite.so');
+                    echo '</br>'."UPDATE $faune set date_import = datetime('now') where date_import is null and uuid_obs = '".$row_[2]."';".'</br>'; //
+                    $results_write_gpkg = $db->query("UPDATE $faune set date_import = datetime('now') where date_import is null and uuid_obs = '".$row_[2]."';"); //
+                    pg_prepare($dbconn, "sql_down", "UPDATE $suivi_point_faune set gpkg_updated = true where uuid_obs = $1;");
+                    if ($results_write_gpkg) {
+                        pg_execute($dbconn, "sql_down",array($row_[2])) or die ( pg_last_error());
+                        $cmd2 = 'sudo -u www-data php /var/www/html/nextcloud/occ files:scan -p "'.$row[3].'"';
+                        $output2=[];
+                        $return_var2=0;
+                        exec($cmd2, $output2, $return_var2);
+                        if ($return_var2 !== 2) {
+                            echo 'scan impossible -_-';
+                        }
                     }
                 }
-            }
+        }
     } else {
         echo '<span style="font-weight:900;color:#bc2020">FAILED</span></br>';
     }
