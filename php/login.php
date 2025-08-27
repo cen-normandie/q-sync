cdc17c5a42b7a03533d6c63904231c9b76de8114
Benno <b.perceval@superceval.fr>
2025-08-27 10:00:30 +0200

    Update login.php

--- php/login.php
+++ php/login.php
@@ -49,7 +49,7 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
                 if($_SESSION['is_equipe_si']) {
                     header ('location: ../write_table.php');
                 } else {
-                    header ('location: index.php');
+                    header ('location: ../index.php');
                 }
             } else {
                 echo "LDAP bind failed...";
