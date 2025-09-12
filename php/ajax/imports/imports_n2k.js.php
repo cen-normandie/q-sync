d3444d053ab1e019d0ced5fafedd2f839da84365
Benno <b.perceval@superceval.fr>
2025-09-12 12:05:33 +0200

    Update imports_n2k.js.php

--- php/ajax/imports/imports_n2k.js.php
+++ php/ajax/imports/imports_n2k.js.php
@@ -8,7 +8,7 @@ $personne = pg_execute($dbconn_geo, "sql_select",array()) or die ( pg_last_error
 
 
 $tables_previ = array(' n2k_previ_point ', ' n2k_previ_ligne ', ' n2k_previ_polygone ');
-$tables_realise = array($n2k_real_polygone_gpkg, $n2k_real_point_gpkg, $n2k_real_ligne_gpkg);
+$tables_realise = array(' n2k_realise_point ', ' n2k_realise_ligne ', ' n2k_realise_polygone ');
 
 
 while($row = pg_fetch_row($personne))
@@ -17,8 +17,29 @@ while($row = pg_fetch_row($personne))
   echo 'Import des données n2k pour l\'utilisateur : <span class="text-danger">'.$row[2].'</span> ('.$row[0].' - '.$row[3].')</br>';
   $n2k_gpkg = '/var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/n2k.gpkg';
   if (file_exists($n2k_gpkg)) {
-
     foreach ($tables_previ as $table) {
+        $cmd_='ogr2ogr -f PostgreSQL "PG:user='.$LOGIN_geonature.' host='.$DBHOST_geonature.' dbname='.$DBNAME_geonature.' password='.$PASS_geonature.'" /var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/n2k.gpkg -nln sandbox.n2k_previ -append -sql "SELECT *, \''.$row[0].'\' as courriel, \''.$row[3].'\' as uuid_nx, id_uuid_n2k as id_uuid_n2k_up from '.$table.'  where importe is null" 2>&1';
+        $output_=[];
+        $return_var=0;
+        exec($cmd_, $output_, $return_var);
+        if ($return_var == 0) {
+            //echo '<br>Mise à jour de la colonne "importe" du geopackage </br>';
+            $db = new SQLite3('/var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/n2k.gpkg');
+            $db->loadExtension('mod_spatialite.so');
+            $results_write_gpkg = $db->query("UPDATE $table set importe = datetime('now') where importe is null ;"); //
+            if ($results_write_gpkg) {
+                //echo '</br>Données n2k ( '.$table.' ) importées avec succès ! </br>';
+                echo $db->changes();
+                echo ' données mises à jour dans le gpkg ( '.$table.' )</br>';
+            } else {echo "Erreur sur le gpkg : " . $db->lastErrorMsg(); }
+            $db->close();
+        }
+        else {
+            echo '</br>FAILED try run : '.$cmd_polygone.'</br>';
+        }
+
+    }
+    foreach ($tables_realise as $table) {
         $cmd_='ogr2ogr -f PostgreSQL "PG:user='.$LOGIN_geonature.' host='.$DBHOST_geonature.' dbname='.$DBNAME_geonature.' password='.$PASS_geonature.'" /var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/n2k.gpkg -nln sandbox.n2k_previ -append -sql "SELECT *, \''.$row[0].'\' as courriel, \''.$row[3].'\' as uuid_nx, id_uuid_n2k as id_uuid_n2k_up from '.$table.'  where importe is null" 2>&1';
         $output_=[];
         $return_var=0;
