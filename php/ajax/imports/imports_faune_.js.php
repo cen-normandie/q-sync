6120ad7912fdf5447f516d3a1318b036399ee43e
Benno <b.perceval@superceval.fr>
2025-09-19 15:11:58 +0200

    up_faune trigger

--- php/ajax/imports/imports_faune_.js.php
+++ php/ajax/imports/imports_faune_.js.php
@@ -3,23 +3,23 @@ include '../../properties.php';
 
 
 $dbconn_geo = pg_connect("hostaddr=$DBHOST_geonature port=$PORT_geonature dbname=$DBNAME_geonature user=$LOGIN_geonature password=$PASS_geonature") or die ('Connexion impossible :'. pg_last_error());
-$select = pg_prepare($dbconn_geo, "sql_select", "select courriel, gn_user_name, nom_ad, uuid_nx from $nx_users where observations_gpkg <> 'ø' and (obs_flore > 0 or obs_flore_polygone > 0 );");
-pg_prepare($dbconn_geo, "sql_import_occtax", "select sandbox.import_flore_();");
-//$delete = pg_prepare($dbconn_geo, "sql_delete", "delete from sandbox.obs_flore_tmp ;");
+$select = pg_prepare($dbconn_geo, "sql_select", "select courriel, gn_user_name, nom_ad, uuid_nx from $nx_users where observations_gpkg <> 'ø' and (obs_faune > 0 ) ;");
+pg_prepare($dbconn_geo, "sql_import_occtax", "select sandbox.import_faune_();");
+//$delete = pg_prepare($dbconn_geo, "sql_delete", "delete from sandbox.obs_faune_tmp ;");
 $personne = pg_execute($dbconn_geo, "sql_select",array()) or die ( pg_last_error());
 
 
-$tables_flore = array(' obs_flore ', ' obs_flore_polygone '); //, ' obs_flore_ligne '
+$tables_faune = array(' obs_faune ', ' obs_faune_polygone '); //, ' obs_faune_ligne '
 
 
 while($row = pg_fetch_row($personne))
 {
   echo '</br>----------------------------------------------------------</br>';
-  echo 'Import des données flore pour l\'utilisateur : <span class="text-danger">'.$row[2].'</span> ('.$row[0].' - '.$row[3].')</br>';
+  echo 'Import des données faune pour l\'utilisateur : <span class="text-danger">'.$row[2].'</span> ('.$row[0].' - '.$row[3].')</br>';
   $observations_gpkg = '/var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/observations.gpkg';
   if (file_exists($observations_gpkg)) {
-    foreach ($tables_flore as $table) {
-        $cmd_='ogr2ogr -f PostgreSQL "PG:user='.$LOGIN_geonature.' host='.$DBHOST_geonature.' dbname='.$DBNAME_geonature.' password='.$PASS_geonature.'" /var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/observations.gpkg -nln sandbox.obs_flore -append -sql "SELECT *, \''.$row[0].'\' as courriel, \''.$row[3].'\' as uuid_nx from '.$table.'  where date_import is null" 2>&1';
+    foreach ($tables_faune as $table) {
+        $cmd_='ogr2ogr -f PostgreSQL "PG:user='.$LOGIN_geonature.' host='.$DBHOST_geonature.' dbname='.$DBNAME_geonature.' password='.$PASS_geonature.'" /var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/observations.gpkg -nln sandbox.obs_faune -append -sql "SELECT *, \''.$row[0].'\' as courriel, \''.$row[3].'\' as uuid_nx from '.$table.'  where date_import is null" 2>&1';
         $output_=[];
         $return_var=0;
         exec($cmd_, $output_, $return_var);
@@ -29,7 +29,7 @@ while($row = pg_fetch_row($personne))
             $db->loadExtension('mod_spatialite.so');
             $results_write_gpkg = $db->query("UPDATE $table set date_import = datetime('now') where date_import is null ;"); //
             if ($results_write_gpkg) {
-                //echo '</br>Données flore ( '.$table.' ) importées avec succès ! </br>';
+                //echo '</br>Données faune ( '.$table.' ) importées avec succès ! </br>';
                 echo $db->changes();
                 echo ' données mises à jour dans le gpkg ( '.$table.' )</br>';
             } else {echo "Erreur sur le gpkg : " . $db->lastErrorMsg(); }
@@ -41,10 +41,10 @@ while($row = pg_fetch_row($personne))
 
     }
     
-    //intégration des données de sandbox.obs_flore dans geonature --> occ_tax
+    //intégration des données de sandbox.obs_faune dans geonature --> occ_tax
     $out = pg_execute($dbconn_geo, "sql_import_occtax",array()) or die ( pg_last_error());
     if ($out) {
-        echo 'Import des données flore dans Geonature réussi !</br>';
+        echo 'Import des données faune dans Geonature réussi !</br>';
     } else {
         echo pg_last_error($dbconn_geo).'</br>';
     }
