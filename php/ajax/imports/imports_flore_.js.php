c901ee8667a554374bcd40d4c2c47f3b98826c32
Benno <b.perceval@superceval.fr>
2025-10-03 17:52:47 +0200

    Update imports_flore_.js.php

--- php/ajax/imports/imports_flore_.js.php
+++ php/ajax/imports/imports_flore_.js.php
@@ -27,6 +27,26 @@ while($row = pg_fetch_row($personne))
         if ($return_var == 0) {
             $db = new SQLite3('/var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/observations.gpkg');
             $db->loadExtension('mod_spatialite.so');
+            $results_write_photo_gpkg = $db->query("SELECT photo_url FROM $table WHERE date_import is null and photo_url is not null;"); //
+            if ($results_write_photo_gpkg) {
+                foreach ( $results_write_photo_gpkg as $row_photo ) {
+                    $photo_file_name = explode("photo/", $row_photo['photo_url'])[1];
+                    if ($photo_file_name != '' && file_exists('/var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/photo/'.$photo_file_name) ) {
+                        $cmd_copy_photo = 'cp /var/www/html/nextcloud/data/'.$row[3].'/files/_qfield/photo/'.$photo_file_name.' /home/geoa/geonature/backend/media/attachments/5/';
+                        //echo '</br>'.$cmd_copy_photo.'</br>';
+                        $output_cp=[];
+                        $return_var_cp=0;
+                        exec($cmd_copy_photo, $output_cp, $return_var_cp);
+                        if ($return_var_cp == 0) {
+                            //echo 'Photo copiée avec succès ! </br>';
+                        } else {
+                            echo 'Erreur lors de la copie de la photo ! </br>';
+                        }
+                    }
+                }
+                echo $db->changes();
+                echo ' données mises à jour dans le gpkg ( '.$table.' )</br>';
+            } else {echo "Erreur sur le gpkg : " . $db->lastErrorMsg(); }
             $results_write_gpkg = $db->query("UPDATE $table set date_import = datetime('now') where date_import is null ;"); //
             if ($results_write_gpkg) {
                 //echo '</br>Données flore ( '.$table.' ) importées avec succès ! </br>';
