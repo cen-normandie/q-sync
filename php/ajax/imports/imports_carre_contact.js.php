a3b21c79b15737eaf7b792b8cec770200317c791
Benno <b.perceval@superceval.fr>
2025-10-14 16:53:12 +0200

    Update imports_carre_contact.js.php

--- php/ajax/imports/imports_carre_contact.js.php
+++ php/ajax/imports/imports_carre_contact.js.php
@@ -3,7 +3,7 @@ include '../../properties.php';
 
 
 $dbconn_geo = pg_connect("hostaddr=$DBHOST_geonature port=$PORT_geonature dbname=$DBNAME_geonature user=$LOGIN_geonature password=$PASS_geonature") or die ('Connexion impossible :'. pg_last_error());
-$select = pg_prepare($dbconn_geo, "sql_select", "select courriel, gn_user_name, nom_ad, uuid_nx from $nx_users ;");
+$select = pg_prepare($dbconn_geo, "sql_select", "select courriel, gn_user_name, nom_ad, uuid_nx from $nx_users where observations_gpkg <> 'ø' and (obs_cc> 0);");
 $personne = pg_execute($dbconn_geo, "sql_select",array()) or die ( pg_last_error());
 pg_prepare($dbconn_geo, "sql_down", "UPDATE $suivi_point_cc set gpkg_updated = true where uuid_contact = $1;");
 // ne fait rien pour le moment
@@ -27,7 +27,7 @@ while($row = pg_fetch_row($personne))
     exec($cmd, $output, $return_var);
     if ($return_var !== 2) {
         //importe toutes les obs cc de sandbox.obs_cc dans geonature
-        // ne fait rien pour le moment à part mettre à jour la date d'import date_import
+        // envoi tout dans la table sh.carre_contact
         $out = pg_execute($dbconn_geo, "sql_import_cc_to_occtax",array()) or die ( pg_last_error());
         if ($out) {
             $to_up = pg_execute($dbconn_geo, "sql_up",array($row[3])) or die ( pg_last_error());
