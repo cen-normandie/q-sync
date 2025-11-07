f8a12a1c7a18b49b723324e34c8a54cc9dcbf304
Benno <b.perceval@superceval.fr>
2025-11-07 11:44:06 +0100

    Update import_test.php

--- php/ajax/imports/import_test.php
+++ php/ajax/imports/import_test.php
@@ -8,9 +8,12 @@ error_reporting(E_ALL);
             $db->loadExtension('mod_spatialite.so');
             $results_write_photo_gpkg = $db->query("SELECT photo_url FROM obs_flore WHERE date_import is null and photo_url is not null;"); //
             echo '</br>Import des photos associées aux observations flore :</br>';
+
+
+
             if ($results_write_photo_gpkg) {
                 echo '</br>Début de la copie des photos...</br>';
-                foreach ( $results_write_photo_gpkg as $row_photo ) {
+                while ($row_photo = $results_write_photo_gpkg->fetchArray(SQLITE3_ASSOC)) {
                     echo 'Traitement de la photo : '.$row_photo['photo_url'].'</br>';
                     $photo_file_name = explode("photo/", $row_photo['photo_url'])[1];
                     if ($photo_file_name != '' && file_exists('/var/www/html/nextcloud/data/8ACA0A1C-6E6E-4912-8511-DB8A02F1CA67/files/_qfield/photo/'.$photo_file_name) ) {
